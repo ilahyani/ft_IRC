2b47bf05d6d3e6f8a5bff85eb61ffb2fa027c9a1
Karim Id bouhouch <kid-bouh@e2r8p15.1337.ma>
2023-05-16 20:13:12 +0100

    fix sending message

--- commands/join.cpp
+++ commands/join.cpp
@@ -6,7 +6,7 @@
 /*   By: kid-bouh <kid-bouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 21:08:28 by kid-bouh          #+#    #+#             */
-/*   Updated: 2023/05/12 01:37:10 by kid-bouh         ###   ########.fr       */
+/*   Updated: 2023/05/16 19:50:45 by kid-bouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,25 @@ bool check_channel_name(std::string channel)
     return true;
 }
 
+void    send_to_clients(Channels *ch, client c)
+{
+    int i = 0;
+    std::vector<client> clients = ch->getMembers();
+    std::string message = ":" + c.get_format() + " JOIN :" + ch->getName() + "\n";
+    client owner = ch->getOwner();
+    while (i < (int)clients.size())
+    {
+        send(clients[i].getsocket(), message.c_str(), message.length(), 0);
+        i++;
+    }
+    send(owner.getsocket(), message.c_str(), message.length(), 0);
+}
+
 std::string getClientsChannel(Channels *ch)
 {
     std::string str;
     std::vector<client> clients = ch->getMembers();
+
 
     int i = clients.size() - 1;
     while (i >= 0)
@@ -46,9 +61,12 @@ std::string getClientsChannel(Channels *ch)
 
 void    print_infos_after_join(std::string clients_of_channel, client &client, std::string channel)
 {
-    client.response(" JOIN :" + channel);
-    client.print(":" + client.getHostname() + " " + RPL_NAMREPLY(client.getNickname(), channel, clients_of_channel));
-    client.print(":" + client.getHostname() + " " + RPL_ENDOFNAMES(client.getNickname(), channel));
+    client.response("JOIN :" + channel);
+    client.responsefromServer(RPL_NAMREPLY(client.getNickname(), channel, clients_of_channel));
+    client.responsefromServer(RPL_ENDOFNAMES(client.getNickname(), channel));
+    
+    // client.print(":" + client.getHostname() + " " + );
+    // client.print(":" + client.getHostname() + " " + RPL_ENDOFNAMES(client.getNickname(), channel));
 }
 
 void server::join_to_channel(std::string channel, std::string key, client &client)
@@ -64,11 +82,13 @@ void server::join_to_channel(std::string channel, std::string key, client &clien
                     client.response(ERR_BADCHANNELKEY(client.getNickname()));
                     return ;
                 }
+                send_to_clients(ch, client);
                 ch->addMember(client, true);
                 print_infos_after_join(getClientsChannel(ch), client, channel);
             }
             else if (!ch->isProtected && key.empty())
             {
+                send_to_clients(ch, client);
                 ch->addMember(client, true);
                 print_infos_after_join(getClientsChannel(ch), client, channel);
             }
--- commands/privmsg.cpp
+++ commands/privmsg.cpp
@@ -6,22 +6,11 @@
 /*   By: kid-bouh <kid-bouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 21:18:50 by kid-bouh          #+#    #+#             */
-/*   Updated: 2023/05/13 00:18:20 by kid-bouh         ###   ########.fr       */
+/*   Updated: 2023/05/16 19:24:44 by kid-bouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../server.hpp"
-
-void    server::sendToClient(std::string receiver, std::string nick_or_channel, std::string message, client sender)
-{
-    std::string msg = ":" + sender.get_format() + " PRIVMSG " + nick_or_channel + " :" + message + "\n";
-    if(get_client(receiver))
-    {
-        if (send(get_client(receiver)->getsocket(), msg.c_str(), msg.length(), 0) < 0)
-            throw std::runtime_error("An error occurred while attempting to send a message to the client.\n");
-    }
-    msg.clear();
-}
 
 void server::send_message_to_user(std::string nick, std::string message, client client)
 {
@@ -30,7 +19,7 @@ void server::send_message_to_user(std::string nick, std::string message, client 
         client.response(ERR_NOSUCHNICK(client.getNickname()));
         return ;
     }
-    sendToClient(nick, nick, message, client);
+    sendToClient(nick, nick, message, client, "PRIVMSG");
 }
 
 void server::send_message_to_channel(std::string channel, std::string message, client c)
@@ -38,7 +27,7 @@ void server::send_message_to_channel(std::string channel, std::string message, c
     Channels *ch = getChannel(channel);
     if (ch == NULL || channel != ch->getName())
     {
-        c.response(ERR_NOSUCHCHANNEL(channel));
+        c.response(ERR_NOSUCHCHANNEL(c.getNickname(), channel));
         return ;
     }
 
@@ -49,29 +38,22 @@ void server::send_message_to_channel(std::string channel, std::string message, c
             i++;
         if ((int)members.size() == i)
             break;
-        sendToClient(members[i].getNickname(), channel, message, c);
+        sendToClient(members[i].getNickname(), channel, message, c, "PRIVMSG");
     }
     if (c.getNickname() != ch->getOwnerNickname())
-        sendToClient(ch->getOwnerNickname(), channel, message, c);
+        sendToClient(ch->getOwnerNickname(), channel, message, c, "PRIVMSG");
 }
 
 void server::privmsg(std::vector<std::string> params, std::map<int, client>::iterator client) {
-
-    std::string message;
-
+    
     if (params.size() < 3)
     {
         client->second.response(ERR_NEEDMOREPARAMS(client->second.getNickname()));
         return ;
     }
-    if (params[2][0] != ':')
-        return ;
-    message = params[2].substr(1, params[2].size()) + " ";
-    for (int i = 3; i < (int)params.size(); i++)
-        message += params[i] + " ";
     
     if (params[1][0] == '#')
-        send_message_to_channel(params[1], message, client->second);
+        send_message_to_channel(params[1], params[2], client->second);
     else
-        send_message_to_user(params[1], message, client->second);
+        send_message_to_user(params[1], params[2], client->second);
 }
