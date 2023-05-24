328aba42ef2ce9790878e14aabda43b9585c200c
oussamaqatim <oqatim@student.1337.ma>
2023-05-24 13:50:25 +0100

    merge Commands

--- commands/names.cpp
+++ commands/names.cpp
@@ -2,5 +2,5 @@
 
 void server::names(std::vector<std::string> params, std::map<int, client>::iterator client) {
     (void)params;(void)client;
-    std::cout << "NAMES command called\n";
+    std::cout << "NAMES command called ===> \n";
 }
